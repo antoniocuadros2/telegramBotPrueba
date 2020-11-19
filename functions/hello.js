ebd858344ca809c7be57f632c95b9456fc694121
Antonio Cuadros <antculap99@gmail.com>
2020-11-19 14:19:40 -0800

    arrancará?

--- functions/hello.js
+++ functions/hello.js
@@ -2,19 +2,15 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.TELEGRAMBOTTOKEN);
 
-bot.start(ctx =>{
-    ctx.reply("Bienvenido");
-})
-
-bot.help(ctx =>{
-    ctx.reply("Ayudaaaaaaaaaaaa");
-})
-
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
 
 exports.handler = async function(event, context) {
-    bot.launch();
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
+        body: JSON.stringify({message: ""})
     };
 }

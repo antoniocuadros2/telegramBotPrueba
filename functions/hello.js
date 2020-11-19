4861d2d134dc568b652fa549b7faa60b1cb4bd8b
Antonio Cuadros <antculap99@gmail.com>
2020-11-19 13:36:42 -0800

    probando función welcome del bot, corregido fallo

--- functions/hello.js
+++ functions/hello.js
@@ -3,7 +3,7 @@ const Telegraf = require('telegraf')
 const bot = new Telegraf(process.env.TELEGRAMBOTTOKEN);
 
 bot.start((ctx) =>{
-    ctx.reply("Bienvenido")
+    ctx.reply("Bienvenido");
 })
 
 exports.handler = async function(event, context) {

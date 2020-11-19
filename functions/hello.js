b38fecbdfeae4a6719e18f60283f2cebbe4fc25e
Antonio Cuadros <antculap99@gmail.com>
2020-11-19 13:43:27 -0800

    arrancará?

--- functions/hello.js
+++ functions/hello.js
@@ -9,7 +9,7 @@ bot.start((ctx) =>{
 
 
 exports.handler = async function(event, context) {
-    bot.launch()
+    await bot.handleUpdate(JSON.parse(event.body));
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Hello World"})

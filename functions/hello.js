68629f6759f1aa53def6fb3a2d4717db996cd69c
Antonio Cuadros <antculap99@gmail.com>
2020-11-20 12:30:40 -0800

    arrancará?

--- functions/hello.js
+++ functions/hello.js
@@ -27,34 +27,13 @@ async function sendToUser(chat_id, text) {
   return rp(options);
 }
 
-module.exports.shortbot = async event => {
-  const body = JSON.parse(event.body);
-  const {chat, text} = body.message;
-
-  if (text) {
-    let message = '';
-    try {
-      const result = await getShortUrl(text);
-      message = `Input: ${text}, \nShort: ${result.result_url}`;
-    } catch (error) {
-      message = `Input: ${text}, \nError: ${error.message}`;
-    }
-
-    await sendToUser(chat.id, message);
-  } else {
-    await sendToUser(chat.id, 'Text message is expected.');
-  }
-
-  return { statusCode: 200 };
-};
-
 
 //HANDLER
 exports.handler = async function(event, context) {
     const body = JSON.parse(event.body);
     const {chat, text} = body.message;
   
-    if (text) {
+    if (text) { //Contiene texto el mensaje
       let message = '';
       try {
         const result = await getShortUrl(text);
@@ -64,7 +43,7 @@ exports.handler = async function(event, context) {
       }
   
       await sendToUser(chat.id, message);
-    } else {
+    } else { //no contiene texto el mensaje
       await sendToUser(chat.id, 'Text message is expected.');
     }
   

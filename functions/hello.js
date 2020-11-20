cda404971a585623a4c60e0c8cdb9d95d3e26e1c
Antonio Cuadros <antculap99@gmail.com>
2020-11-20 13:29:48 -0800

    arrancará?

--- functions/hello.js
+++ functions/hello.js
@@ -19,10 +19,26 @@ exports.handler = async function(event, context) {
     let body = JSON.parse(event.body);
     let {chat, text} = body.message;
   
-    if (text){ //Contiene texto el mensaje
-        let message = '';
-
-        enviaMensaje(chat.id, text);
+    if (text){ //Contiene texto el mensaje, será el comando
+        let a_devolver = '';
+        switch (text) {
+            case "/ktengo":
+                a_devolver = "Que tareas tengo que hacer";
+                break;
+            case "/ktengourgente":
+                a_devolver = "Que tareas tengo que hacer urgentemente";
+                break;
+            default:
+                a_devolver = "Usa /ktengo para saber que tareas tienes que realizar y  / "
+                break;
+        }       
+        request({
+            method: 'GET',
+            uri: `https://api.telegram.org/bot${process.env.TELEGRAMBOTTOKEN}/sendMessage`,
+            qs: {
+              chat_id,  //id del chat
+              text      //contenido
+        })
     }
     else{ //no contiene texto el mensaje
         enviaMensaje(chat.id, 'Text message is expected.');

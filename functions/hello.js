397ffc8cf86403850287caebaeddc1d61ae61f68
Antonio Cuadros <antculap99@gmail.com>
2020-11-21 04:57:27 -0800

    probando

--- functions/hello.js
+++ functions/hello.js
@@ -14,22 +14,38 @@ async function enviaMensaje(chat_id, text) {
   return request(options);
 }
 
-function getAsignaturas(){
+function getAsignaturas(asignatura = "todas"){
   num_asignaturas = 3;
   a_devolver = "";
   
   for(i = 1; i <= num_asignaturas; i++){
     tareas = "";
     fechas = ""
-    if (data['asignaturas'][i]["tareas"].length > 1){
-      for( j = 0; j < data['asignaturas'][i]["tareas"].length; j++){
-        tareas += "   \n     Tarea: " +  data['asignaturas'][i]["tareas"][j] + " Fecha: " + data['asignaturas'][i]["fecha_tareas"][j] + " ";
+    if(asignatura == "todas"){
+      if (data['asignaturas'][i]["tareas"].length > 1){
+        for( j = 0; j < data['asignaturas'][i]["tareas"].length; j++){
+          tareas += "   \n     Tarea: " +  data['asignaturas'][i]["tareas"][j] + " Fecha: " + data['asignaturas'][i]["fecha_tareas"][j] + " ";
+        }
       }
+      else{
+        tareas +=   "   \n     Tarea: " + data['asignaturas'][i]["tareas"] + " Fecha: " + data['asignaturas'][i]["fecha_tareas"];
+      }
+      a_devolver += data['asignaturas'][i]["nombreAsignatura"]  + "-> " +  tareas  + "\n";
     }
     else{
-      tareas +=   "   \n     Tarea: " + data['asignaturas'][i]["tareas"] + " Fecha: " + data['asignaturas'][i]["fecha_tareas"];
+      if (data['asignaturas'][i]["nombreAsignatura"] == asignatura){
+        if (data['asignaturas'][i]["tareas"].length > 1){
+          for( j = 0; j < data['asignaturas'][i]["tareas"].length; j++){
+            tareas += "   \n     Tarea: " +  data['asignaturas'][i]["tareas"][j] + " Fecha: " + data['asignaturas'][i]["fecha_tareas"][j] + " ";
+          }
+        }
+        else{
+          tareas +=   "   \n     Tarea: " + data['asignaturas'][i]["tareas"] + " Fecha: " + data['asignaturas'][i]["fecha_tareas"];
+        }
+        a_devolver += data['asignaturas'][i]["nombreAsignatura"]  + "-> " +  tareas  + "\n";
+      }
     }
-    a_devolver += data['asignaturas'][i]["nombreAsignatura"]  + "-> " +  tareas  + "\n";
+
   }
   return a_devolver;
 }
